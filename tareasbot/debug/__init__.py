14f62101bcbbcd27538b1888968f2d1092897dcb
Guacha <guachamalinga@gmail.com>
2021-09-27 10:13:47 -0500

    Console Refactoring

--- tareasbot/debug/__init__.py
+++ tareasbot/debug/__init__.py
@@ -1,41 +1,46 @@
 import datetime
-class Console():
-    
-    show_debug = True
-    show_log = True
-    show_warnings = True
-    show_errors = True
-    
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    
-    @staticmethod
-    def separator():
-        print(f"{Console.BOLD}==================================================================================================================={Console.ENDC}")
-    
-    @staticmethod
-    def log(msg, module="MAIN"):
+
+
+class Console:
+
+    def __init__(self, module_name: str,
+                 debug: bool = True,
+                 log: bool = True,
+                 warnings: bool = True,
+                 errors: bool = True):
+        self.module = module_name
+        self.show_debug = debug
+        self.show_log = log
+        self.show_warnings = warnings
+        self.show_errors = errors
+
+        self.HEADER = '\033[95m'
+        self.OKBLUE = '\033[94m'
+        self.OKCYAN = '\033[96m'
+        self.OKGREEN = '\033[92m'
+        self.WARNING = '\033[93m'
+        self.FAIL = '\033[91m'
+        self.ENDC = '\033[0m'
+        self.BOLD = '\033[1m'
+        self.UNDERLINE = '\033[4m'
+
+    def separator(self):
+        print(
+            f"{self.BOLD}========================================================================================"
+            f"========================================================================================{self.ENDC}")
+
+    def log(self, msg):
         now = datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")
-        print(f"[{now}][LOG][{module}]: {msg}")
-        
-    @staticmethod
-    def warn(msg, module="MAIN"):
+        print(f"[{now}][LOG][{self.module}]: {msg}")
+
+    def warn(self, msg):
         now = datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")
-        print(f"{Console.WARNING}[{now}][WARNING][{module}]: {msg}{Console.ENDC}")
-        
-    @staticmethod
-    def debug_log(msg, module="MAIN"):
+        print(f"{self.WARNING}[{now}][WARNING][{self.module}]: {msg}{self.ENDC}")
+
+    def debug_log(self, msg):
         now = datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")
-        print(f"{Console.OKBLUE}[{now}][DEBUG][{module}]: {msg}{Console.ENDC}")
-        
-    @staticmethod
-    def err(msg, module="MAIN"):
+        print(f"{self.OKBLUE}[{now}][DEBUG][{self.module}]: {msg}{self.ENDC}")
+
+    def err(self, msg, module="MAIN"):
         now = datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")
-        print(f"{Console.FAIL}[{now}][WARNING][{module}]: {msg}{Console.ENDC}")
+        print(f"{self.FAIL}[{now}][WARNING][{module}]: {msg}{self.ENDC}")
